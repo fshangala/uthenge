851a9b4e147ca4055bf076e80c67d4b032907a16
fshangala <fshangala@gmail.com>
2024-09-04 08:52:19 +0200

    the opposite

--- src/routes/auth/hooks/is_unauthenticated.jsx
+++ src/routes/auth/hooks/is_unauthenticated.jsx
@@ -2,15 +2,15 @@ import { useContext, useEffect } from "react";
 import { UserContext } from "../../../contexts/user_context";
 import { useNavigate } from "react-router-dom";
 
-export default function useIsAuthenticated() {
+export default function useIsUnauthenticated() {
   const user = useContext(UserContext)
   const navigate = useNavigate()
 
   useEffect(function(){
-    if(!user.session) {
-      navigate("/login/")
+    if(user.session) {
+      navigate("/")
     }
   })
 
-  return user.session !== null
+  return user.session === null
 }
